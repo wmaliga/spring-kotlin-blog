17fc5b95ef31189a969fd35ed5ada8f3797c57b4
wmaliga <wojciech.maliga@gmail.com>
2021-02-23 18:57:20 +0100

    Article list added

--- src/main/kotlin/com/wojtek/blog/controller/HtmlController.kt
+++ src/main/kotlin/com/wojtek/blog/controller/HtmlController.kt
@@ -1,16 +1,18 @@
 package com.wojtek.blog.controller
 
+import com.wojtek.blog.repository.ArticleRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class HtmlController {
+class HtmlController(private val articleRepository: ArticleRepository) {
 
     @GetMapping("/")
     fun blog(model: Model): String {
         model["title"] = "Blog"
+        model["articles"] = this.articleRepository.findAllByOrderByAddedDesc()
         return "blog"
     }
 }
